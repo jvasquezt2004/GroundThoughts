94680ccbd95ef7c15a2076ae071a3c01d99e50bb
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-06-20 21:19:13 -0600

    Created get users api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,8 +4,10 @@ import {
   updateUser,
   deleteUser,
   signout,
+  getUsers,
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifiedUser.js";
+import { get } from "mongoose";
 
 const router = express.Router();
 
@@ -13,5 +15,6 @@ router.get("/test", test);
 router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 export default router;
