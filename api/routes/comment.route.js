600402261f92352bc64dd53069e3cfe86399f89a
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-06-21 22:52:46 -0600

    Show coments of the post

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifiedUser.js";
-import { createComment } from "../controllers/comment.controller.js";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, createComment);
+router.get("/getpostcomments/:postId", getPostComments);
 
 export default router;
